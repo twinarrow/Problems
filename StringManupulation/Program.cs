31d83cfe99bbbe967f1130c33a515b1aca98790d
Arul Selvan S <arulselvan_s@persistent.co.in>
2017-08-08 14:32:19 +0530

    First commit, addeding few proj

---
+++ StringManupulation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManupulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string[] input = Console.ReadLine().Split(' ');
+            GetInputs();
+            reverse();
+            output();
+
+        }
+
+        static string str, revstring;
+        static int length;
+        private static void reverse()
+        {
+            length = str.Length - 1;
+            while (length >=0)
+            {
+                revstring = revstring + str[length];
+                length--;
+            }
+        }
+
+        public static void GetInputs()
+        {
+            str = Console.ReadLine();
+        }
+
+        private static  void output()
+        {
+            Console.WriteLine("Thre reveresed is : {0}", revstring);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Thread/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            start();
+        }
+
+        private static void start()
+        {
+            CallWithAsync();
+        }
+
+        private async static void CallWithAsync()
+        {
+            //some other tasks
+            string result = await GreetingAsync("Bulbul");
+            //We can add  multiple "await" in same "async" method
+            //string result1 = await GreetingAsync("Ahmed");
+            //string result2 = await GreetingAsync("Every Body");
+            Console.WriteLine(result);
+        }
+        static Task<string> GreetingAsync(string name)
+        {
+            return Task.Run<string>(() =>
+            {
+                return Greeting(name);
+            });
+        }
+        static string Greeting(string name)
+        {
+            Parallel.For()
+            Thread.Sleep(3000);
+
+            return string.Format("Hello, {0}", name);
+
+        }
+    }
+}
